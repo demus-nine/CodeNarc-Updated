091f39141b8f43a92dd9df3548733a8d3fc56559
Daniel Demus <dde@nine.dk>
2020-01-03 21:14:03 +0100

    Improve replacement process

--- src/main/java/org/codenarc/idea/CodeNarcComponentObsoleter.java
+++ src/main/java/org/codenarc/idea/CodeNarcComponentObsoleter.java
@@ -3,40 +3,61 @@ package org.codenarc.idea;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginInstaller;
 import com.intellij.ide.plugins.PluginManager;
-import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.ide.plugins.PluginNode;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.components.BaseComponent;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.progress.EmptyProgressIndicator;
 import com.intellij.openapi.updateSettings.impl.PluginDownloader;
+import com.intellij.openapi.updateSettings.impl.UpdateChecker;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 
 //import com.intellij.ide.plugins.PluginManagerConfigurable;
 
 public class CodeNarcComponentObsoleter implements BaseComponent {
 
+    private static final String codenarcId = "CodeNarc";
+
     public void initComponent() {
         try {
-            PluginId codenarcId = PluginId.getId("CodeNarc");
-            IdeaPluginDescriptor codenarcPlugin;
-            if (!PluginManagerCore.isPluginInstalled(codenarcId)) {
-                codenarcPlugin = new PluginNode(codenarcId);
-                PluginDownloader downloader = PluginDownloader.createDownloader(codenarcPlugin);
+            boolean willInstallCodenarclugin = false;
+            boolean restart = false;
+            IdeaPluginDescriptor codenarcDescriptor;
+            PluginId codenarcPlugin = PluginId.getId(codenarcId);
+            if (!PluginManager.isPluginInstalled(codenarcPlugin)) {
+                codenarcDescriptor = new PluginNode(codenarcPlugin);
+                PluginDownloader downloader = PluginDownloader.createDownloader(codenarcDescriptor);
                 downloader.prepareToInstall(new EmptyProgressIndicator());
-                PluginInstaller.installAfterRestart(downloader.getFile(), true, null, codenarcPlugin);
+                PluginInstaller.installAfterRestart(downloader.getFile(), true, null, codenarcDescriptor);
+                willInstallCodenarclugin = restart = true;
             }
-            else {
-                codenarcPlugin = PluginManager.getPlugin(codenarcId);
-                if (codenarcPlugin != null && !codenarcPlugin.isEnabled()) {
-                    codenarcPlugin.setEnabled(true);
+
+            if (PluginManager.isDisabled(codenarcId)) {
+                PluginManager.enablePlugin(codenarcId);
+                restart = true;
+            }
+
+            if (!willInstallCodenarclugin) {
+                Optional<PluginDownloader> updateDownloader = UpdateChecker.getPluginUpdates().stream().filter(d -> d.getPluginId().equals(codenarcId)).findFirst();
+                if (updateDownloader.isPresent()) {
+                    PluginDownloader downloader = updateDownloader.get();
+                    downloader.prepareToInstall(new EmptyProgressIndicator());
+                    PluginInstaller.installAfterRestart(downloader.getFile(), true, PluginManager.getPlugin(codenarcPlugin).getPath(), downloader.getDescriptor());
                 }
             }
-            IdeaPluginDescriptor codenarcUpdatedPlugin = PluginManager.getPlugin(PluginId.getId("dk.demus.idea.CodeNarc"));
-            PluginInstaller.prepareToUninstall(Objects.requireNonNull(codenarcUpdatedPlugin));
-            ApplicationManagerEx.getApplicationEx().restart(true);
+
+            if (restart || (PluginManager.isPluginInstalled(codenarcPlugin) && !PluginManager.isDisabled(codenarcId))) {
+                IdeaPluginDescriptor codenarcUpdatedPlugin = PluginManager.getPlugin(PluginId.getId("dk.demus.idea.CodeNarc"));
+                PluginInstaller.prepareToUninstall(Objects.requireNonNull(codenarcUpdatedPlugin));
+                restart = true;
+            }
+
+            if (restart) {
+                ApplicationManagerEx.getApplicationEx().restart(true);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
